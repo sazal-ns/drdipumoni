0945fa442bc8e739a6d587f8ce0a6946145527ac
Siddiqui_RTsoftBD <siddiqui.sazal@gmail.com>
2017-07-05 19:21:55 +0600

    db day 1

--- app/src/main/java/com/rtsoftbd/siddiqui/drDipuMoni/helper/LocalDB.java
+++ app/src/main/java/com/rtsoftbd/siddiqui/drDipuMoni/helper/LocalDB.java
@@ -1,19 +1,22 @@
 
 /*
  * Copyright By Noor Nabiul Alam Siddiqui on Behalf of RTsoftBD
+ * (C) 7/5/17 7:21 PM
  *  www.fb.com/sazal.ns
  *  _______________________________________
  *    Name:     DipuMoni
- *    Created at:  7/5/17 5:46 PM
- *    Updated at: 7/5/17 5:42 PM
+ *    Updated at: 7/5/17 7:20 PM
  *  ________________________________________
  */
 
 package com.rtsoftbd.siddiqui.drDipuMoni.helper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.rtsoftbd.siddiqui.drDipuMoni.model.AboutSocial;
 
 
 public class LocalDB extends SQLiteOpenHelper {
@@ -32,10 +35,55 @@ public class LocalDB extends SQLiteOpenHelper {
     private static final String KEY_SECTION_ABOUT_TITLE = "section_about_title";
     private static final String KEY_SECTION_ABOUT_HEADER = "sector_about_header";
     private static final String KEY_SELECTION_ABOUT_DATA = "section_about_data";
-    private static final String KEYS_SELECTION_ABOUT_LINK = "section_about_link";
+    private static final String KEY_SELECTION_ABOUT_LINK = "section_about_link";
+    private static final String KEY_SELECTION_ABOUT_PICTURE = "section_about_pic";
+    private static final String KEY_SOCIAL_LINK1_NAME = "slink1_name";
+    private static final String KEY_SOCIAL_LINK1_URL = "slink1_link";
+    private static final String KEY_SOCIAL_LINK2_NAME = "slink2_name";
+    private static final String KEY_SOCIAL_LINK2_URL = "slink2_link";
+    private static final String KEY_SOCIAL_LINK3_NAME = "slink3_name";
+    private static final String KEY_SOCIAL_LINK3_URL = "slink3_link";
+    private static final String KEY_ADMIN_EMAIL = "adminEmail";
+    private static final String KEY_COPYRIGHT = "copy_right";
 
 
+    /*CREATING TABLE*/
+    /*CREATE MANAGE TABLE*/
+    /*###ADD PICTURE TO DB REMAINING######*/
+    private static final String CREATE_TABLE_MANAGE = "CREATE TABLE " + TABLE_MANAGE +" ("+
+            KEY_ID + " INTEGER PRIMARY KEY,"+
+            KEY_SECTION_ABOUT_TITLE + " TEXT,"+
+            KEY_SECTION_ABOUT_HEADER + " TEXT,"+
+            KEY_SELECTION_ABOUT_DATA + " TEXT,"+
+            KEY_SELECTION_ABOUT_LINK + " TEXT,"+
+            KEY_SOCIAL_LINK1_NAME + " TEXT,"+
+            KEY_SOCIAL_LINK1_URL + " TEXT,"+
+            KEY_SOCIAL_LINK2_NAME + " TEXT,"+
+            KEY_SOCIAL_LINK2_URL + " TEXT,"+
+            KEY_SOCIAL_LINK3_NAME + " TEXT,"+
+            KEY_SOCIAL_LINK3_URL + " TEXT,"+
+            KEY_ADMIN_EMAIL + " TEXT,"+
+            KEY_COPYRIGHT + " TEXT )";
 
+    public long insertMange(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_SECTION_ABOUT_TITLE, AboutSocial.getSectionAboutTitle());
+        values.put(KEY_SECTION_ABOUT_HEADER, AboutSocial.getSectorAboutHeader());
+        values.put(KEY_SELECTION_ABOUT_DATA, AboutSocial.getSectionAboutData());
+        values.put(KEY_SELECTION_ABOUT_LINK, AboutSocial.getSectionAboutLink());
+        values.put(KEY_SOCIAL_LINK1_NAME,AboutSocial.getSlink1Name());
+        values.put(KEY_SOCIAL_LINK1_URL, AboutSocial.getSlink1Link());
+        values.put(KEY_SOCIAL_LINK2_NAME, AboutSocial.getSlink2Name());
+        values.put(KEY_SOCIAL_LINK2_URL, AboutSocial.getSlink2Link());
+        values.put(KEY_SOCIAL_LINK3_NAME, AboutSocial.getSlink3Name());
+        values.put(KEY_SOCIAL_LINK3_URL,AboutSocial.getSlink3Link());
+        values.put(KEY_ADMIN_EMAIL, AboutSocial.getAdminEmail());
+        values.put(KEY_COPYRIGHT, AboutSocial.getCopyRight());
+
+        return db.insert(TABLE_MANAGE, null, values);
+    }
 
     public LocalDB(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -43,11 +91,13 @@ public class LocalDB extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE_MANAGE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MANAGE);
 
+        onCreate(db);
     }
 }
